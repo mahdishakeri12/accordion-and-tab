56d4985b013b95a44b4a7cf43ae39f8ed8726f7f
mahdi shakeri <mahdishakeri431@gmail.com>
2025-07-27 18:48:13 +0330

    tabs added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-    $('.title').click(function(){
+$(document).ready(function () {
+    $('.title').click(function () {
         $(this).closest('.acc').find('.content').slideToggle(200)
+    })
+    $('.tabs ul li a').click(function (e) {
+        e.preventDefault();
+        $('.tabs ul li a').removeClass('active')
+        $(this).addClass('active');
     })
 })
